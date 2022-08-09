aba97807803165bfa3d63668fb27a3a695df8d88
martindputra <martin.putra@moladin.com>
2022-08-09 10:36:55 +0700

    removed unneccessary comments

--- mini_projects/src/cpp/native-lib.cpp
+++ mini_projects/src/cpp/native-lib.cpp
@@ -12,13 +12,12 @@ extern "C" jobject Java_com_tanakayu_mini_1projects_di_NdkModule_adrNativeValues
     jmethodID init = env->GetMethodID(mapClass, "<init>", "(I)V");
     jmethodID put = env->GetMethodID(mapClass, "put", "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;");
 
-    jsize map_len = 4; // WARNING, MAKE SURE YOU CHANGE THIS WHEN YOU ADD/DELETE NEW ENTRIES
+    jsize map_len = 4;
     jobject hashMap = env->NewObject(mapClass, init, map_len);
     env->CallObjectMethod(hashMap, put, env->NewStringUTF("KEY_A"), env->NewStringUTF("Some Value"));
     env->CallObjectMethod(hashMap, put, env->NewStringUTF("KEY_B"), env->NewStringUTF("https://your.company.dev-url.com"));
     env->CallObjectMethod(hashMap, put, env->NewStringUTF("KEY_C"), env->NewStringUTF("https://your.company.staging-url.com"));
-    env->CallObjectMethod(hashMap, put, env->NewStringUTF("KEY_D"), env->NewStringUTF("https://your.company.production-urln.com/"));
-    // WARNING, MAKE SURE TO INCREMENT map_len VARIABLE AFTER YOU ADD/DELETE NEW ENTRIES
+    env->CallObjectMethod(hashMap, put, env->NewStringUTF("KEY_D"), env->NewStringUTF("https://your.company.production-url.com/"));
 
     return hashMap;
 }
